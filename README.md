173076d8c52e77a74f963898eca17982b4461991
Hassan Haidar <hassan.haidar@mail.mcgill.ca>
2025-11-05 20:15:56 -0500

    Initialize chat server

---
+++ README.md
@@ -0,0 +1,6 @@
+# ChatServer
+
+A ChatServer built in Go for the purpose of learning Go.
+
+Source:
+https://www.thepolyglotdeveloper.com/2016/12/create-real-time-chat-app-golang-angular-2-websockets/
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gorilla/websocket"
+)
+
+type ClientManager struct {
+	clients    map[*Client]bool // client pointer to boolean
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+type Client struct {
+	id     string
+	socket *websocket.Conn
+	send   chan []byte
+}
+
+type Message struct {
+	Sender    string `json:"sender,omitempty"`
+	Recipient string `json:"recipient,omitempty"`
+	Content   string `json:"content,omitempty"`
+}
+
+//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
+func main() {
+	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
+	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
+	s := "gopher"
+	fmt.Printf("Hello and welcome, %s!\n", s)
+
+	for i := 1; i <= 5; i++ {
+		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
+		fmt.Println("i =", 100/i)
+	}
+}
